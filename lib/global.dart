f2c873a0ff79e08ac17cecd81a75eb5add5f0cd1
Shubham Bichkar <shubhamb.softprodigy@gmailcom>
2021-12-10 13:12:49 +0530

    Changes

--- lib/global.dart
+++ lib/global.dart
@@ -2,9 +2,9 @@ import 'package:logger/logger.dart';
 
 const bool debug = true;
 
-// const BASE_URL = "https://dev-v2.tabletop-cloud.com";
+const BASE_URL = "https://dev-v2.tabletop-cloud.com";
 // const BASE_URL = "https://prod-v2.tabletop-cloud.com";
-const BASE_URL = "https://stage-v2.tabletop-cloud.com";
+// const BASE_URL = "https://stage-v2.tabletop-cloud.com";
 
 const ANDROID_APP_ID = 'co.tabletop.app';
 const APPLE_APP_ID = '1541981454';
