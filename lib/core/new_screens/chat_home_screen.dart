d462542350261b1987c01fb6193185fa88381ac5
Shubham Bichkar <shubhamb.softprodigy@gmailcom>
2022-01-03 17:51:53 +0530

    Rive Anim Incomplete Implementation

--- lib/core/new_screens/chat_home_screen.dart
+++ lib/core/new_screens/chat_home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
 import 'package:provider/provider.dart';
+import 'package:rive/rive.dart';
 import 'package:ttp_chat/core/new_screens/chat_error_screen.dart';
 import 'package:ttp_chat/core/new_screens/rooms_list.dart';
 import 'package:ttp_chat/core/new_screens/search_user/search_user_screen.dart';
@@ -13,6 +15,8 @@ import 'package:ttp_chat/core/new_screens/widgets/helpers.dart';
 import 'package:ttp_chat/core/new_screens/widgets/start_chat_message.dart';
 import 'package:ttp_chat/core/screens/loading_screen.dart';
 import 'package:ttp_chat/core/widgets/input_search.dart';
+import 'package:ttp_chat/core/widgets/rive_anim.dart';
+import 'package:ttp_chat/core/widgets/rive_anim_controller.dart';
 import 'package:ttp_chat/features/chat/presentation/chat_provider.dart';
 import 'package:ttp_chat/utils/functions.dart';
 
@@ -69,12 +73,46 @@ class _ChatHomeScreenState extends State<_ChatHomeScreen> {
 
   late Stream<List<types.Room>> stream;
 
+  String stateChangeMessage = '';
+
+  Artboard? _riveArtboard;
+  StateMachineController? _controller;
+  SMIInput<bool>? _trigger;
+
   @override
   void initState() {
+
+    rootBundle.load('assets/loader_anim.riv').then(
+          (data) async {
+        // Load the RiveFile from the binary data.
+        final file = RiveFile.import(data);
+
+        // The artboard is the root of the animation and gets drawn in the
+        // Rive widget.
+        final artboard = file.mainArtboard;
+        var controller = StateMachineController.fromArtboard(
+          artboard,
+          'State Machine 1',
+          onStateChange: _onStateChange,
+        );
+        if (controller != null) {
+          artboard.addController(controller);
+          _trigger = controller.findInput('Trigger 1');
+        }
+        setState(() => _riveArtboard = artboard);
+      },
+    );
+
     chatProvider = context.read<ChatProvider>();
     initializeFlutterFire();
+
     super.initState();
   }
+
+  void _onStateChange(String stateMachineName, String stateName) => setState(
+        () => stateChangeMessage =
+    'State Changed in $stateMachineName to $stateName',
+  );
 
   @override
   void dispose() {
---
+++ lib/core/widgets/rive_anim.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
+import 'package:ttp_chat/global.dart';
+
+class RiveAnim extends StatefulWidget {
+  final String? riveFileName;
+  final RiveAnimationController? animationController;
+  final double height;
+  final double width;
+
+  RiveAnim({
+    this.riveFileName,
+    this.animationController,
+    this.height = 100,
+    this.width = 100,
+  });
+
+  @override
+  _RiveAnimState createState() => _RiveAnimState();
+}
+
+class _RiveAnimState extends State<RiveAnim> {
+  Artboard? _artBoard;
+
+  void _loadRiveFile() async {
+    try {
+      var bytes = await rootBundle.load(widget.riveFileName!);
+      final file = RiveFile.import(bytes);
+
+      setState(() {
+        _artBoard = file.mainArtboard
+          ..addController(widget.animationController!);
+        widget.animationController!.isActive = true;
+      });
+    } catch (e, s) {
+      logger.e('Error loading Rive animation', e, s);
+    }
+  }
+
+  @override
+  void initState() {
+    _loadRiveFile();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: widget.height,
+      width: widget.width,
+      child: _artBoard == null ? const SizedBox() : Rive(artboard: _artBoard!),
+    );
+  }
+}
