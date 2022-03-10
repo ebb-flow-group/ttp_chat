c6aacf4b5cbf971b924db4c40348b8488606d9d5
gauravcalidig <gaurav.yadav@calidig.com>
2022-03-10 12:44:27 +0530

    Order Navigation from Chat

--- lib/core/screens/home/home_widgets/room_list_view.dart
+++ lib/core/screens/home/home_widgets/room_list_view.dart
@@ -34,11 +34,10 @@ class RoomListView extends StatelessWidget {
         Container(
           margin: const EdgeInsets.symmetric(horizontal: 17.0),
           child: InputSearch(
-            hintText: 'Search a brand, user or mobile',
-            onChanged: (String value) {
-              chatProvider.searchUsers(value);
-            },
-          ),
+              hintText: 'Search a brand, user or mobile',
+              onChanged: (String value) {
+                chatProvider.searchUsers(value);
+              }),
         ),
         const SizedBox(height: 17),
         HomeTabs(
