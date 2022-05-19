a01bb16c9e49f5130891103656acd8e9fc2c182a
Gaurav <gaurav.yadav@calidig.com>
2022-05-19 13:10:47 +0530

    Fix: Not Subscribing to fcm topics on web

--- lib/core/services/notification_service.dart
+++ lib/core/services/notification_service.dart
@@ -3,10 +3,14 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:ttp_chat/utils/functions.dart';
 
 class ChatNotificationService {
   static subscribeToChannel(Map<String, dynamic> room) {
+    //TODO handle Subcribing to topic on Web
+    if (kIsWeb) return;
+
     ///Not Subcribing the owner of the room
     if (room['owner'] == FirebaseAuth.instance.currentUser?.uid) return;
     String? topic = room['topicName'];
@@ -17,6 +21,9 @@ class ChatNotificationService {
   }
 
   static subscribeToAllChannels() {
+    //TODO handle Subcribing to topic on Web
+    if (kIsWeb) return;
+
     FirebaseFirestore.instance
         .collection('rooms')
         .where('userIds', arrayContains: FirebaseAuth.instance.currentUser!.uid)
@@ -38,20 +45,23 @@ class ChatNotificationService {
     });
   }
 
-  static Future unSubscribeFromAllChannels() async{
+  static Future unSubscribeFromAllChannels() async {
+    //TODO handle Subcribing to topic on Web
+    if (kIsWeb) return;
+
     if (FirebaseAuth.instance.currentUser == null) return;
     QuerySnapshot<Map<String, dynamic>> data = await FirebaseFirestore.instance
         .collection('rooms')
         .where('userIds', arrayContains: FirebaseAuth.instance.currentUser!.uid)
         .where("type", isEqualTo: "channel")
         .get();
-        for (var doc in data.docs) {
-        Map<String, dynamic> room = doc.data();
-        String? topic = room['topicName'];
-        if (topic != null) {
-          log('UnSubscribing From topic: $topic');
-          FirebaseMessaging.instance.unsubscribeFromTopic(topic);
-        }
+    for (var doc in data.docs) {
+      Map<String, dynamic> room = doc.data();
+      String? topic = room['topicName'];
+      if (topic != null) {
+        log('UnSubscribing From topic: $topic');
+        FirebaseMessaging.instance.unsubscribeFromTopic(topic);
       }
+    }
   }
 }
