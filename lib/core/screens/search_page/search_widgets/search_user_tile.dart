95c20122f36b7c598b46a57239129cc3d70a7706
GauravCalidig <gaurav.yadav@calidig.com>
2022-06-03 11:11:28 +0530

    Fix: Migrated `/home-brand` route to use username as param

--- lib/core/screens/search_page/search_widgets/search_user_tile.dart
+++ lib/core/screens/search_page/search_widgets/search_user_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import 'package:route_parser/route_parser.dart';
 
 import '../../../../features/chat/domain/search_user_model.dart';
 import '../../../../utils/functions.dart';
@@ -21,7 +22,7 @@ class SearchUserTile extends StatelessWidget {
         : GestureDetector(
             onTap: () {
               if (!GetIt.I<ChatUtils>().isCreatorsApp && (user.uid != null) && user.type == UserType.brand) {
-                context.push(Routes.homeOutletDetailPage, extra: user.uid);
+                context.push(RouteParser(Routes.homeOutletPage).reverse({'id': user.uid!}));
               }
             },
             child: Padding(
--- lib/core/services/routes.dart
+++ lib/core/services/routes.dart
@@ -1,5 +1,5 @@
 class Routes {
-  static const String homeOutletDetailPage = '/home-brand';
+  static const String homeOutletPage = '/home-brand/:id';
   static const String orderDetailRoute = '/orders/detail/:id';
   static const String orderPaymentRoute = '/orders/pay/:id';
   static const String ordersRoute = '/orders';
